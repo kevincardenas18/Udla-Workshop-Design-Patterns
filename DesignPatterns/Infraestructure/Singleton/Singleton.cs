e6b5fa9436ac572cf3205184ff46ae3f30b1bced
Kevin Cárdenas <kevin.cardenas-1@outlook.com>
2023-07-03 22:04:17 -0500

    Initial commit

---
+++ DesignPatterns/Infraestructure/Singleton/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using DesignPatterns.Models;
+using System.Collections.Generic;
+
+namespace DesignPatterns.Infraestructure.Singleton
+{
+    public class MemoryCollection
+    {
+        public ICollection<Vehicle> Vehiculos { get; set; }
+
+        private static MemoryCollection Instance;
+        public MemoryCollection()
+        {
+            Vehiculos = new List<Vehicle>();
+        }
+        public MemoryCollection GetInstance()
+        {
+            if (Instance == null)
+            {
+                Instance = new MemoryCollection();
+
+
+            }
+
+            return Instance;
+        }
+    }
+}
---
+++ DesignPatterns/Repositories/MyVehiclesRepository.cs
@@ -0,0 +1,36 @@
+﻿using DesignPatterns.Infraestructure.Singleton;
+using DesignPatterns.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Repositories
+{
+    public class MyVehiclesRepository : IVehicleRepository
+    {
+        private readonly MemoryCollection _memoryCollection;
+
+        public MyVehiclesRepository()
+        {
+            _memoryCollection = MemoryCollection.GetInstance();
+        }
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            _memoryCollection.Vehiculos.Add(vehicle);
+        }
+
+        public Vehicle Find(string id)
+        {
+            return _memoryCollection.Vehiculos.FirstOrDefault(v => v.ID.Equals(new Guid(id)));
+        }
+
+        public ICollection<Vehicle> GetVehicles()
+        {
+            return _memoryCollection.Vehiculos;
+        }
+
+
+    }
+}
